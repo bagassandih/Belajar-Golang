6f95d78eec6f11c350fae9e74ff87f3e27b59507
bagassandih <bagassandi13@gmail.com>
2022-07-24 20:31:32 +0700

    Latihan Mandiri, sensor kata umpatan

--- string/latihan_sensor_kata.go
+++ string/latihan_sensor_kata.go
@@ -5,52 +5,97 @@ import (
 	"strings"
 	)
 
-//sensor kata berdasarkan index string	
-func sensorKataIndex(kata string) string{
-	var hasil string
-	for i := 0; i < len(kata)/2; i++ {
-	hasil = strings.Replace(kata, string(kata[i]), "*", -1) 
-	kata = hasil
-	}
-	return kata
-}
+// //sensor kata berdasarkan index string	
+// func sensorKataIndex(kata string) string{
+// 	var hasil string
+// 	for i := 0; i < len(kata)/2; i++ {
+// 	hasil = strings.Replace(kata, string(kata[i]), "*", -1) 
+// 	kata = hasil
+// 	}
+// 	return kata
+// }
 
 
-func sensorKataVokal(kata string) string{
-	var hasil, vokalCek string
-	vokal := [5]string {"a", "i", "u", "e", "o"}
+// func sensorKataVokal(kata string) string{
+// 	var hasil, vokalCek string
+// 	vokal := [5]string {"a", "i", "u", "e", "o"}
 
 	
+// 	for _, daftarVokal := range vokal{
+// 	cek :=strings.Contains(kata, daftarVokal)
+// 	if cek == true {
+// 		vokalCek = daftarVokal
+// 		hasil = strings.Replace(kata, vokalCek, "*", -1) 
+// 		}
+// 	}			
+
+// 	kata = hasil
+// 	return kata
+// }
+
+
+func cekSensor (kata string) string{
+
+	hasil := kata //default untuk antisipasi jika tidak disensor
+	var vokalCek string
+	vokal := [5]string {"a", "i", "u", "e", "o"}
+	sensor := false
+	KataSensor := [...]string { //daftar kata umpatan
+		"anjing",
+		"asu",
+		"bego",
+		"goblok",
+		"bangsat",
+	}
+	
+
+// to lower agar menjadi kecil semua dan mudah di scan
+	kata = strings.ToLower(kata)	
+// cek kata harus disensor atau tidak
+	for _, daftarKataSensor := range KataSensor {
+		cekKata := strings.Contains(kata, daftarKataSensor)
+		if cekKata == true { sensor=true }
+	}
+
+// menyensor kata jika ada kata umpatan
+	if sensor == true {
 	for _, daftarVokal := range vokal{
 	cek :=strings.Contains(kata, daftarVokal)
 	if cek == true {
 		vokalCek = daftarVokal
 		hasil = strings.Replace(kata, vokalCek, "*", -1) 
+			}
+
 		}
-	//fmt.Println(kata, i, daftarVokal, cek, vokalCek, hasil )
-
-	 
-
-	}
-				
-
+	}	
+	// kembalikan huruf besar di awal kata
+	Kapital := strings.ToUpper( string(hasil[0]) ) 
+	hasil = Kapital + hasil[1:len(hasil)]
 	kata = hasil
 	return kata
 }
 
 func main(){
-	// SEKEDAR CONTOH BUKAN UNTUK DI TIRU
-	fmt.Println("Sensor Index")
-	fmt.Println( sensorKataIndex("Bangsat") )
-	fmt.Println( sensorKataIndex("Anjing") )
-	fmt.Println( sensorKataIndex("Asu lu") )
-	fmt.Println()
+	// SEKEDAR CONTOH BUKAN UNTUK DI TIRU KATA KATANYA
+	// fmt.Println("Sensor Index")
+	// fmt.Println( sensorKataIndex("Bangsat") )
+	// fmt.Println( sensorKataIndex("Anjing") )
+	// fmt.Println( sensorKataIndex("Asu lu") )
+	// fmt.Println()
 
-	fmt.Println("Sensor vokal")
-	fmt.Println( sensorKataVokal("Bangsat") )
-	fmt.Println( sensorKataVokal("Anjing") )
-	fmt.Println( sensorKataVokal("Asu lu") )
+	// fmt.Println("Sensor vokal")
+	// fmt.Println( sensorKataVokal("Bangsat") )
+	// fmt.Println( sensorKataVokal("Anjing") )
+	// fmt.Println( sensorKataVokal("Asu lu") )
+	// fmt.Println()
 
-	
+	fmt.Println("Cek Kata Umpatan dengan Sensor Huruf Vokal")
+	fmt.Println( cekSensor("Bangsat") )
+	fmt.Println( cekSensor("Anjing") )
+	fmt.Println( cekSensor("Asu lu") )
+	fmt.Println( cekSensor("Serigala berbulu domba") )
+	fmt.Println( cekSensor("Malam Minggu ") )
+	fmt.Println( cekSensor("Kamu memang bego!") )
+	fmt.Println()	
 
 }
